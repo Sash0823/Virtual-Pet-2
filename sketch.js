c13bc7ca22d780b3a15d7f997f62365868cebb83
Sash0823 <67402337+Sash0823@users.noreply.github.com>
2020-09-13 20:48:51 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database;
+
+var dog;
+dog.setAnimation("cartoon-dog-names");
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
+
+
+
